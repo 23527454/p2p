15991b66ed10db37d0f0d2e1b4b613b700d6bb6a
gzd_glb <23527454_glb@qq.com>
2020-02-04 14:47:44 +0800

    首次提交（静态素材）

---
+++ src/main/java/com/demo/p2p/P2pApplication.java
@@ -0,0 +1,14 @@
+package com.demo.p2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class P2pApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P2pApplication.class, args);
+    }
+
+}
