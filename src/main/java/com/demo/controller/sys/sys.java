ac917ec6efacadd35a50082e1ba573e84b8afd74
Administrator <lxc>
2020-02-05 16:22:27 +0800

    lxc

--- src/main/java/com/demo/controller/sys/sys.java
+++ src/main/java/com/demo/controller/sys/sys.java
@@ -1,5 +1,15 @@
 package com.demo.controller.sys;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class sys {
+
+    @RequestMapping(value = "/index")
+    public String index(){
+        System.out.println("index=====");
+        return "index";
+    }
 
 }
