2bd6239ff65f4b2d71669e36e360b5314db8c23c
Administrator <lxc>
2020-02-05 17:15:31 +0800

    Merge branch 'master' of https://github.com/23527454/p2p
    # Conflicts:
    #	src/main/java/com/demo/p2p/controller/SysController.java

--- src/main/java/com/demo/p2p/controller/SysController.java
+++ src/main/java/com/demo/p2p/controller/SysController.java
@@ -12,5 +12,35 @@ public class SysController {
         return "index";
     }
 
+    /**
+     * 跳转公告详情
+     * @return
+     */
+    @RequestMapping(value = "/xiangqing")
+    public String xiangqing(){
+        return "公司公告详细";
+    }
 
+    /**
+     * 网站公告
+     * @return
+     */
+    @RequestMapping(value = "/gonggao")
+    public String gonggao(){
+        return "网站公告";
+    }
+
+    /**
+     * 推荐项目
+     * @return
+     */
+    @RequestMapping(value = "/tuijian")
+    public String tuijian(){
+        return "list";
+    }
+
+    @RequestMapping(value = "/infor")
+    public String infor(){
+        return "infor";
+    }
 }
