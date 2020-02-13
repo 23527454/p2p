7863a7d94e16bb35ab9d74b86f5c7c5c621a5d84
Administrator <lxc>
2020-02-13 09:43:25 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/demo/p2p/controller/SysController.java
    #	src/main/java/com/demo/p2p/controller/UsersController.java
    #	src/main/java/com/demo/p2p/mapper/UsersMapper.java
    #	src/main/java/com/demo/p2p/service/UsersService.java
    #	src/main/java/com/demo/p2p/service/impl/UsersServiceImpl.java

--- src/main/java/com/demo/p2p/controller/LogController.java
+++ src/main/java/com/demo/p2p/controller/LogController.java
@@ -16,9 +16,11 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequestMapping("/log")
 public class LogController {
+
  @RequestMapping(value = "Test3")
     public String Test(){
      return "Test";
  }
+
 }
 
