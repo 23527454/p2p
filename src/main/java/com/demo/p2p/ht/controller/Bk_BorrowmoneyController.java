d458eacfbbff443e015b09e01652910a5d4f0b37
gzd_glb <23527454_glb@qq.com>
2020-02-24 17:22:54 +0800

    完成后台财务管理——借贷信息列表

--- src/main/java/com/demo/p2p/ht/controller/Bk_BorrowmoneyController.java
+++ src/main/java/com/demo/p2p/ht/controller/Bk_BorrowmoneyController.java
@@ -1,12 +1,17 @@
 package com.demo.p2p.ht.controller;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.demo.p2p.ht.entity.Borrowmoney;
 import com.demo.p2p.ht.service.Bk_BorrowmoneyService;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -22,8 +27,22 @@ public class Bk_BorrowmoneyController {
     @Resource
     private Bk_BorrowmoneyService borrowmoneyService;
 
+    @RequestMapping(value = "/bajax")
+    @ResponseBody
+    public Borrowmoney bajax(Integer id){
+        return borrowmoneyService.getById(id);
+    }
+
     @RequestMapping(value = "/hjyList")
-    public String hjyList(){
+    public String hjyList(Integer current, Model model){
+        if(current==null || current==0){
+            current=1;
+        }
+        Page<Borrowmoney> page=new Page<>(current,5);
+        IPage<Borrowmoney> iPage=borrowmoneyService.page(page);
+        List<Borrowmoney> list=iPage.getRecords();
+        model.addAttribute("page",iPage);
+        model.addAttribute("wdlist",list);
         return "view/Borrowmoneylist";
     }
 }
