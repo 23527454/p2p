d2dd349f8ad5a804a2a6ac6595bf5df700a99bd4
XuJunYi383 <2966945427@qq.com>
2020-02-25 15:42:12 +0800

    提现模块（5）

---
+++ src/main/java/com/demo/p2p/service/impl/PoundageServiceImpl.java
@@ -0,0 +1,20 @@
+package com.demo.p2p.service.impl;
+
+import com.demo.p2p.entity.Poundage;
+import com.demo.p2p.mapper.PoundageMapper;
+import com.demo.p2p.service.PoundageService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 手续费记录表 服务实现类
+ * </p>
+ *
+ * @author gzd
+ * @since 2020-02-25
+ */
+@Service
+public class PoundageServiceImpl extends ServiceImpl<PoundageMapper, Poundage> implements PoundageService {
+
+}
