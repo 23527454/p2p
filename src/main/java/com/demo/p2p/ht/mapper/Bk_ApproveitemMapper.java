0e48249024c260a1bd438cc34d3c0b06874381f5
LTX <1031694969@qq.com>
2020-02-28 17:34:26 +0800

    ltx_test

--- src/main/java/com/demo/p2p/ht/mapper/Bk_ApproveitemMapper.java
+++ src/main/java/com/demo/p2p/ht/mapper/Bk_ApproveitemMapper.java
@@ -3,6 +3,9 @@ package com.demo.p2p.ht.mapper;
 import com.demo.p2p.ht.entity.Approveitem;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * <p>
  * 认证项管理 Mapper 接口
@@ -12,5 +15,5 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2020-02-27
  */
 public interface Bk_ApproveitemMapper extends BaseMapper<Approveitem> {
-
+    public List<Approveitem> selList(Map<String, Object> map);
 }
--- src/main/java/com/demo/p2p/ht/service/Bk_ApproveitemService.java
+++ src/main/java/com/demo/p2p/ht/service/Bk_ApproveitemService.java
@@ -2,6 +2,10 @@ package com.demo.p2p.ht.service;
 
 import com.demo.p2p.ht.entity.Approveitem;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.demo.p2p.ht.entity.Withdrawal;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -12,5 +16,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2020-02-27
  */
 public interface Bk_ApproveitemService extends IService<Approveitem> {
-
+    public List<Approveitem> selList(Map<String, Object> map);
 }
