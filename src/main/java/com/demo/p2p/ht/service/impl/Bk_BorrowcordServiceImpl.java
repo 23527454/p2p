3944187b7977e11c2c4a609820bf8ab768eb7b9a
XuJunYi383 <2966945427@qq.com>
2020-03-07 11:17:07 +0800

    后台首页  已完成

--- src/main/java/com/demo/p2p/ht/service/impl/Bk_BorrowcordServiceImpl.java
+++ src/main/java/com/demo/p2p/ht/service/impl/Bk_BorrowcordServiceImpl.java
@@ -6,6 +6,9 @@ import com.demo.p2p.ht.service.Bk_BorrowcordService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 /**
  * <p>
  * 还款记录表 服务实现类
@@ -17,4 +20,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class Bk_BorrowcordServiceImpl extends ServiceImpl<Bk_BorrowcordMapper, Borrowcord> implements Bk_BorrowcordService {
 
+    @Resource
+    private Bk_BorrowcordMapper bk_borrowcordMapper;
+
+    @Override
+    public List<Borrowcord> lssum() {
+        return bk_borrowcordMapper.lssum();
+    }
 }
