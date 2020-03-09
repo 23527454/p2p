d15ea12f614484a9ddceccde697441729dd20b6a
gzd_glb <23527454_glb@qq.com>
2020-03-09 16:23:01 +0800

    完成后台忘记密码功能

--- src/main/java/com/demo/p2p/controller/UsersController.java
+++ src/main/java/com/demo/p2p/controller/UsersController.java
@@ -2,9 +2,9 @@ package com.demo.p2p.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.cloopen.rest.sdk.CCPRestSmsSDK;
 import com.demo.p2p.entity.*;
 import com.demo.p2p.service.*;
+import com.demo.p2p.util.SendMessage;
 import org.apache.shiro.authc.DisabledAccountException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,14 +17,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.mail.internet.MimeMessage;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.Instant;
 import java.time.ZoneId;
-import java.util.*;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 /**
  * <p>
@@ -47,6 +49,8 @@ public class UsersController {
     private CertifrecordService certifrecordService;
     @Resource
     private CertificationService certificationService;
+
+    private SendMessage sendMessage=new SendMessage();
 
     //发邮件
     @Autowired
@@ -217,79 +221,10 @@ public class UsersController {
     @RequestMapping(value = "/sendMessages")
     @ResponseBody
     public Object sendMessages(String phone){
-        //发送短信验证码
-        HashMap<String, Object> result = null;
-        HashMap<String,Object> map=new HashMap<>();
-
-        //初始化SDK
-        CCPRestSmsSDK restAPI = new CCPRestSmsSDK();
-
-        //******************************注释*********************************************
-        //*初始化服务器地址和端口                                                       *
-        //*沙盒环境（用于应用开发调试）：restAPI.init("sandboxapp.cloopen.com", "8883");*
-        //*生产环境（用户应用上线使用）：restAPI.init("app.cloopen.com", "8883");       *
-        //*******************************************************************************
-        restAPI.init("app.cloopen.com", "8883");
-
-        //******************************注释*********************************************
-        //*初始化主帐号和主帐号令牌,对应官网开发者主账号下的ACCOUNT SID和AUTH TOKEN     *
-        //*ACOUNT SID和AUTH TOKEN在登陆官网后，在“应用-管理控制台”中查看开发者主账号获取*
-        //*参数顺序：第一个参数是ACOUNT SID，第二个参数是AUTH TOKEN。                   *
-        //*******************************************************************************
-        restAPI.setAccount("8aaf07086c6b60c5016c6c1af0c40075", "29d09f2154f34c979d05c4b8ddaa203b");
-
-
-        //******************************注释*********************************************
-        //*初始化应用ID                                                                 *
-        //*测试开发可使用“测试Demo”的APP ID，正式上线需要使用自己创建的应用的App ID     *
-        //*应用ID的获取：登陆官网，在“应用-应用列表”，点击应用名称，看应用详情获取APP ID*
-        //*******************************************************************************
-        restAPI.setAppId("8aaf07086c6b60c5016c6c1af120007b");
-
-
-        //******************************注释****************************************************************
-        //*调用发送模板短信的接口发送短信                                                                  *
-        //*参数顺序说明：                                                                                  *
-        //*第一个参数:是要发送的手机号码，可以用逗号分隔，一次最多支持100个手机号                          *
-        //*第二个参数:是模板ID，在平台上创建的短信模板的ID值；测试的时候可以使用系统的默认模板，id为1。    *
-        //*系统默认模板的内容为“【云通讯】您使用的是云通讯短信模板，您的验证码是{1}，请于{2}分钟内正确输入”*
-        //*第三个参数是要替换的内容数组。																														       *
-        //**************************************************************************************************
-
-        //**************************************举例说明***********************************************************************
-        //*假设您用测试Demo的APP ID，则需使用默认模板ID 1，发送手机号是13800000000，传入参数为6532和5，则调用方式为           *
-        //*result = restAPI.sendTemplateSMS("13800000000","1" ,new String[]{"6532","5"});																		  *
-        //*则13800000000手机号收到的短信内容是：【云通讯】您使用的是云通讯短信模板，您的验证码是6532，请于5分钟内正确输入     *
-        //*********************************************************************************************************************
-        String tel=phone;
         Random rd=new Random();
         int sjs=(int)rd.nextInt(9999);
         String yzm=String.valueOf(sjs);
-        String time="5";
-
-        result = restAPI.sendTemplateSMS(tel,"1" ,new String[]{yzm,time});
-
-        System.out.println("SDKTestGetSubAccounts result=" + result);
-        if("000000".equals(result.get("statusCode"))){
-            Cookie cookie1 = new Cookie("yzm",yzm);
-            cookie1.setMaxAge(60*Integer.parseInt(time));
-            //正常返回输出data包体信息（map）
-            HashMap<String,Object> data = (HashMap<String, Object>) result.get("data");
-            Set<String> keySet = data.keySet();
-            for(String key:keySet){
-                Object object = data.get(key);
-                System.out.println(key +" = "+object);
-                map.put("result",true);
-                map.put("yzm",yzm);
-            }
-        }else{
-            //异常返回输出错误码和错误信息
-            System.out.println("错误码=" + result.get("statusCode") +" 错误信息= "+result.get("statusMsg"));
-            map.put("result",false);
-            map.put("messages",result.get("statusMsg"));map.put("result",true);
-            map.put("yzm",yzm);
-        }
-        return map;
+        return sendMessage.sendPhoneMessage(phone,yzm);
     }
 
     /**
---
+++ src/main/java/com/demo/p2p/util/RandomCharacterAndNumber.java
@@ -0,0 +1,25 @@
+package com.demo.p2p.util;
+
+import java.util.Random;
+
+public class RandomCharacterAndNumber {
+    public String getRandomCharacterAndNumber(int length) {
+        String val = "";
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            String charOrNum = random.nextInt(2) % 2 == 0 ? "char" : "num"; // 输出字母还是数字
+
+            if ("char".equalsIgnoreCase(charOrNum)) // 字符串
+            {
+                int choice = random.nextInt(2) % 2 == 0 ? 65 : 97; // 取得大写字母还是小写字母
+                val += (char) (choice + random.nextInt(26));
+                // int choice = 97; // 指定字符串为小写字母
+                val += (char) (choice + random.nextInt(26));
+            } else if ("num".equalsIgnoreCase(charOrNum)) // 数字
+            {
+                val += String.valueOf(random.nextInt(10));
+            }
+        }
+        return val;
+    }
+}
